3cd68443e51be559844538b3052de877a7713fa0
JosephPark <human1654@gmail.com>
2020-04-02 21:41:16 +0900

    Initial Commit [~to Jump motion]

---
+++ Assets/02_Scripts/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float boundX;
+    [SerializeField] private float boundY;
+    float camSpeed = 5.0f;
+
+    private void Update()
+    {
+        ClampCamera();
+    }
+
+    void ClampCamera()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 playerPos = player.transform.position;
+        Vector3 clampedPos = new Vector3(
+            Mathf.Clamp(cursorPos.x, playerPos.x - boundX, playerPos.x + boundX),
+            Mathf.Clamp(cursorPos.y, 1.65f, playerPos.y + boundY),
+            -10
+            );
+        Vector3 cameraPos = Vector3.Lerp(
+            transform.position,
+            clampedPos,
+            camSpeed * Time.deltaTime);
+        transform.position = cameraPos;
+    }
+}
---
+++ Assets/02_Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 8f;
+    private Rigidbody2D rb;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Transform feetPos;
+    [SerializeField] private float checkRadius;
+    private bool isStepping;
+    public LayerMask objectStepping;
+
+    private Vector2 characterScale;
+    private Animator animator;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        characterScale = transform.localScale;
+    }
+    private void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float inputH = Input.GetAxisRaw("Horizontal");
+        Vector2 direction = new Vector2(inputH, 0);
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+        AnimateHorizontal(inputH);
+    }
+
+    private void AnimateHorizontal(float inputH)
+    {
+        animator.SetFloat("moveSpeed", Mathf.Abs(inputH));
+        Flip(inputH);
+    }
+
+    private void Flip(float inputH)
+    {
+        if (inputH < 0)
+            characterScale.x = -1;
+        else
+            characterScale.x = 1;
+        transform.localScale = characterScale;
+    }
+
+    private void Jump()
+    {
+        isStepping = Physics2D.OverlapCircle(
+            feetPos.position,
+            checkRadius,
+            objectStepping
+            );
+        if (isStepping == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        AnimateVertical(isStepping);
+    }
+
+    private void AnimateVertical(bool isStepping)
+    { 
+        animator.SetBool("isStepping", isStepping);
+    }
+    
+    
+}
